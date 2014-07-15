e2a82b797636e0ead12013f20ee73fada84e76da
Kyle Frank <kfrank@mix.wvu.edu>
2014-07-15 18:46:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CyberBadgesApp::Application.config.secret_token = '66ac33e2a4bd828cb1bfb709e7671ef98fed382007a7dcf55b4148b9cb542bdbf2f014718c298dc1ebd1b16b4d37edfa73d8be8d9dd009605f9ac094a2cc3e1b'
